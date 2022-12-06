16ba5fe019dd05a30016dd0a5d29d1a05f17cb1b
Abdullah Bakış <99887425+abdullahbakis@users.noreply.github.com>
2022-12-06 20:13:30 +0300

    The End v1.2

--- src/main/java/com/build/stockdataapplication/controller/StockController.java
+++ src/main/java/com/build/stockdataapplication/controller/StockController.java
@@ -133,6 +133,7 @@ public class StockController {
 
         listStocks = new ArrayList<>();
 
+
         listStocks.add(stocks0);
         listStocks.add(stocks1);
         listStocks.add(stocks2);
